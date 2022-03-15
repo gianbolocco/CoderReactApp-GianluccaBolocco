47a25351b4ce1ee2b4ec7838721a3127431384ba
gianlucca <gianluccabolocco@gmail.com>
2022-03-15 01:06:10 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.scss';
+
+import * as React from 'react';
+import NavBar from './components/NavBar';
+
+
+
+
+function App() {
+  return (
+
+    // JSX
+
+    <div className="App">
+    
+     <NavBar/>
+
+    <div className='header'></div>
+    </div>
+
+  );
+}
+
+export default App;
